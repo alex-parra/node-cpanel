465f8f250e304ebc6f8e94541e5f75f186552d90
Alex Parra <alexpds@gmail.com>
2021-03-19 10:07:43 +0000

    mode cluster

--- pm2-prod.config.js
+++ pm2-prod.config.js
@@ -4,6 +4,8 @@ module.exports = {
       name: 'node-cpanel',
       script: './src/server.js',
       watch: false,
+      exec_mode: 'cluster',
+      instances: 1,
       env: {
         NODE_ENV: 'production',
       },

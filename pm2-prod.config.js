3fd10a2a8db6d289103ad8f272dd9a6d93575af4
Alex Parra <alexpds@gmail.com>
2021-03-19 10:11:07 +0000

    remove cluster mode

--- pm2-prod.config.js
+++ pm2-prod.config.js
@@ -4,8 +4,6 @@ module.exports = {
       name: 'node-cpanel',
       script: './src/server.js',
       watch: false,
-      exec_mode: 'cluster',
-      instances: 1,
       env: {
         NODE_ENV: 'production',
       },
